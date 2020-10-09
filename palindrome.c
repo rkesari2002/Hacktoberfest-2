b0efb3a7a7d03f8ce4d1b2489f992a699cdf2cf1
rkesari2002 <37709936+rkesari2002@users.noreply.github.com>
2020-10-09 15:19:08 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+    int n, r, sum = 0, temp;
+    printf("enter the number=");
+    scanf("%d", &n);
+    temp = n;
+    while (n > 0) {
+        r = n % 10;
+        sum = (sum * 10) + r;
+        n = n / 10;
+    }
+    if (temp == sum)
+        printf("this is a palindrome number ");
+    else
+        printf("this number is not palindrome number");
+    return 0;
+}
